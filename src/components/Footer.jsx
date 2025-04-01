bdcccf49605708f044a4feac1a74d095aef12554
tommaso.dare <tommaso.dare96@gmail.com>
2025-04-01 15:13:27 +0200

    Refactor App component by creating separate Header, Main, and Footer components for better structure and maintainability

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+
+    return (
+
+        <>
+            <footer>
+
+                <div className="bg-info text-center py-3">
+                    <p className="mb-0 text-white">© 2023 My Website. All rights reserved.</p>
+                    <div>
+                        <a href="#" className="text-white mx-2">Privacy Policy</a>
+                        <a href="#" className="text-white mx-2">Terms of Service</a>
+                        <a href="#" className="text-white mx-2">Contact Us</a>
+                    </div>
+                </div>
+
+            </footer>
+
+        </>
+
+    )
+
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+export default function Main() {
+
+    return (
+
+        <>
+
+            <main>
+
+                <div className="jumbotron bg-dark">
+                    <h1 className="text-white text-center">This is my Website</h1>
+                </div>
+
+            </main>
+
+
+        </>
+
+
+    )
+
+
+}
